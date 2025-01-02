2e16dd5a469dfae01c960aaadfc8b42f9293f2be
kafkaarko <kafkaarko@gmail.com>
2025-01-03 01:13:26 +0700

    fix migration

---
+++ database/migrations/2025_01_02_030115_create_stock_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gudang', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('location')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('kategori_item', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['Running','Futsal','Sneakers','Canvas']);
+            $table->enum('category', ['Olahraga', 'Santai']);
+            $table->integer('size');
+            $table->enum('color', ['Black', 'White', 'Blue', 'Brown']);
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('gudang_id');
+            $table->unsignedBigInteger('kategori_id');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('gudang_id')->references('id')->on('gudang')->onDelete('cascade');
+            $table->foreign('kategori_id')->references('id')->on('kategori_item')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock');
+    }
+};
